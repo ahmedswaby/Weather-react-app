e233827d5eacff3b610ceff94338917e352216e7
Ahmed Khaled <47076615+ahmedswaby@users.noreply.github.com>
2019-01-30 11:51:07 +0200

    Add files via upload

---
+++ precache-manifest.aabcfb7afc09265686f46ed183de8823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather-react-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "d01157d07cdf0ece631f",
+    "url": "/weather-react-app/static/js/runtime~main.d01157d0.js"
+  },
+  {
+    "revision": "cbb3e3c332b68b5e680c",
+    "url": "/weather-react-app/static/js/main.cbb3e3c3.chunk.js"
+  },
+  {
+    "revision": "50db6bdfe2d7b0d8fbab",
+    "url": "/weather-react-app/static/js/1.50db6bdf.chunk.js"
+  },
+  {
+    "revision": "cbb3e3c332b68b5e680c",
+    "url": "/weather-react-app/static/css/main.35383a5e.chunk.css"
+  },
+  {
+    "revision": "c435e78f87445e45e8f4f3c9eb564903",
+    "url": "/weather-react-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ahmedswaby/Weather-react-app/precache-manifest.342e307933ffee63fe7a7b0bf6bc1059.js"
+  "/weather-react-app/precache-manifest.aabcfb7afc09265686f46ed183de8823.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ahmedswaby/Weather-react-app/index.html", {
+workbox.routing.registerNavigationRoute("/weather-react-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
